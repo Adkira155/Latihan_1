ae6b81c10a13c20728d4f0d5294d0dfa2ab85c31
Adkira155 <150764874+Adkira155@users.noreply.github.com>
2023-11-14 11:50:13 +0800

    Add files via upload

---
+++ home-soalukk.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/home.css">
+</head>
+<body>
+<div class="container">
+    <div class="navbar">
+            <nav>
+                    <ul>
+                    <li><a href="#beranda">Beranda</a></li>
+                    <li><a href="#layanan kami">Layanan Kami</a></li>
+                    <li><a href="#Tentang kami">Tentang Kami</a></li>
+                    <li><a href="#Kontak kami">Kontak Kami</a></li>
+                    </ul>
+            </nav>
+    </div>
+
+        <div class="banner">
+            <header >
+            <p>Selamat Datang Di Website Kami!</p>
+            </header>
+        </div>
+
+    <div class="row">
+
+            <aside class="kiri">
+                <div class="sidebar">
+                    <a href="">Artikel</a>
+                    <a href="">Bantuan</a>
+                    <a href="">Kuesioner</a>
+                    <a href="">Login</a>
+                    <a href="">Register</a>
+                </div>
+            </aside>
+
+            <main class="kanan">
+                <div class="konten-utama">
+                    <div class="column">
+                        <img src="img/Car Wash.png" alt="">
+                        <br>
+                        <br>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ad explicabo quibusdam at id laboriosam, eos iste. Blanditiis esse similique praesentium eius. Tempore, aperiam obcaecati earum voluptate error ipsam id!</p>
+                        <br>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum reprehenderit atque officiis blanditiis, magnam pariatur possimus esse fugit id repellat, in ab laborum unde quidem porro quam soluta! Recusandae, repellat.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quibusdam ut est, minus cupiditate itaque ullam et quos vel? Ex, iusto iste voluptate dolorum ullam eum nesciunt consequuntur aliquid repellendus.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus reprehenderit laboriosam ipsa sequi sunt veniam illum earum delectus, vitae magnam dolor sapiente, harum iste quia quo fugit, blanditiis iusto vero!
+                        </p>
+                    </div>
+                </div>
+            </main>
+        
+    </div>
+
+            <footer>
+            <p>	Copyright &copy; 2023 - SMKN 2 Kota Banjarmasin - Adinda Ridha Salsabila</p>
+            </footer>
+</div>
+</body>
+</html>
---
+++ ukk2.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/home.css">
+</head>
+<body>
+<div class="container">
+    <div class="navbar">
+            <nav>
+                    <ul>
+                    <li><a href="#beranda">Beranda</a></li>
+                    <li><a href="#layanan kami">Layanan Kami</a></li>
+                    <li><a href="#Tentang kami">Tentang Kami</a></li>
+                    <li><a href="#Kontak kami">Kontak Kami</a></li>
+                    </ul>
+            </nav>
+    </div>
+
+        <div class="banner">
+            <header >
+            <p>Selamat Datang Di Website Kami!</p>
+            </header>
+        </div>
+
+    <div class="row">
+
+            <aside class="kiri">
+                <div class="sidebar">
+                    <a href="">Artikel</a>
+                    <a href="">Bantuan</a>
+                    <a href="">Kuesioner</a>
+                    <a href="">Login</a>
+                    <a href="">Register</a>
+                </div>
+            </aside>
+
+            <main class="kanan">
+                <div class="konten-utama">
+                    <div class="column2">
+                    <form action="" method="post">
+                    <div style="margin-top: 20px;" class="box form">
+
+                            <div class="form1">
+                            <input placeholder="Nama Depan" type="text">
+                            <input placeholder="Nama Belakang" type="text">
+                            <input placeholder="Email" type="email">
+                            </div>
+                            <div class="form2">
+                            <input class="form22" placeholder="Username" type="text">
+                            </div>
+                            <div class="form1">
+                            <input class="form22" placeholder="Password" type="password">
+                            <input class="form22" placeholder="Confirm Password" type="password">
+                            </div>
+                           
+
+                            <!-- Input Data -->
+                            <center>
+                            <div class="tombol">
+                                <input class="btn" type="submit" name="Login" value="Tombol" >
+                                <input class="btn" type="submit" name="submit" value="Tombol" id=""> 
+                            </div>
+                            </center>
+                        </form>
+<br>   
+                    </div>
+                </div>
+            </main>
+        
+    </div>
+
+            <footer>
+            <p>	Copyright &copy; 2023 - SMKN 2 Kota Banjarmasin - Adinda Ridha Salsabila</p>
+            </footer>
+</div>
+</body>
+</html>
